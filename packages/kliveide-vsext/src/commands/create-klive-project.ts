28e5d599d4a751578612f25776e9af0a503a3873
Istvan Novak <dotneteer@hotmail.com>
2020-08-11 09:38:12 +0200

    fix: do not allow Create Klive Project with no folder open

--- packages/kliveide-vsext/src/commands/create-klive-project.ts
+++ packages/kliveide-vsext/src/commands/create-klive-project.ts
@@ -19,52 +19,78 @@ export const PACMAN_TAPE = "pac-man.tzx";
  */
 export function createKliveProject(context: vscode.ExtensionContext): void {
   const folders = vscode.workspace.workspaceFolders;
-  const projFolder = folders ? folders[0].uri.fsPath : "";
+  const projFolder = folders ? folders[0].uri.fsPath : null;
+  if (!projFolder) {
+    vscode.window.showWarningMessage(
+      "Please open a project folder before creating a Klive project. No Klive project has been created yet."
+    );
+    return;
+  }
+
   const templateFolder = path.join(context.extensionPath, TEMPLATE_PATH);
+
+  let foldersCreated = 0;
+  let filesCreated = 0;
 
   // --- Create the .spectrum folder and its contents
   const spectrumFolder = path.join(projFolder, SPECTRUM_FOLDER);
   if (!fs.existsSync(spectrumFolder)) {
     fs.mkdirSync(spectrumFolder, { recursive: true });
+    foldersCreated++;
   }
   const memFile = path.join(spectrumFolder, MEMORY_FILE);
   if (!fs.existsSync(memFile)) {
     copyFile(path.join(templateFolder, MEMORY_FILE), memFile);
+    filesCreated++;
   }
   const disassFile = path.join(spectrumFolder, DISASSEMBLY_FILE);
   if (!fs.existsSync(disassFile)) {
     copyFile(path.join(templateFolder, DISASSEMBLY_FILE), disassFile);
+    filesCreated++;
   }
   const basicFile = path.join(spectrumFolder, BASIC_FILE);
   if (!fs.existsSync(basicFile)) {
     copyFile(path.join(templateFolder, BASIC_FILE), basicFile);
+    filesCreated++;
   }
 
   // --- Create the tape folder and its contents
   const tapeFolder = path.join(projFolder, TAPE_FOLDER);
   if (!fs.existsSync(tapeFolder)) {
     fs.mkdirSync(tapeFolder, { recursive: true });
+    foldersCreated++;
   }
 
   // --- Create the code folder and its contents
   const codeFolder = path.join(projFolder, CODE_FOLDER);
   if (!fs.existsSync(codeFolder)) {
     fs.mkdirSync(codeFolder, { recursive: true });
+    foldersCreated++;
   }
   const jetSetFile = path.join(tapeFolder, JETSET_TAPE);
   if (!fs.existsSync(jetSetFile)) {
     copyFile(path.join(templateFolder, JETSET_TAPE), jetSetFile);
+    filesCreated++;
   }
   const jungleFile = path.join(tapeFolder, JUNGLE_TAPE);
   if (!fs.existsSync(jungleFile)) {
     copyFile(path.join(templateFolder, JUNGLE_TAPE), jungleFile);
+    filesCreated++;
   }
   const pacManFile = path.join(tapeFolder, PACMAN_TAPE);
   if (!fs.existsSync(pacManFile)) {
     copyFile(path.join(templateFolder, PACMAN_TAPE), pacManFile);
+    filesCreated++;
   }
 
-  vscode.window.showInformationMessage("Klive project created.");
+  // --- Provide the result message
+  let message = "The current project folder is already a Klive project. No new files or folders were added.";
+  if (filesCreated > 0 || foldersCreated > 0) {
+    message = `Klive project created with ${foldersCreated || "no"} new folder${
+      foldersCreated > 1 ? "s" : ""
+    } and ${filesCreated || "no"} new file${filesCreated > 1 ? "s" : ""}.`;
+  }
+  vscode.window.showInformationMessage(message);
 }
 
 export function copyFile(src: string, dest: string): void {
