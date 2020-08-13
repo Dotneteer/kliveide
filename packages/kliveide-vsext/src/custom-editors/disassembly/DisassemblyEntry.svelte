9657cb7d875321b9303d53d99341a5be786f7e7d
Dotneteer <dotneteer@hotmail.com>
2020-08-13 16:33:45 +0200

    feat: display annotations with disassembly lines

--- packages/kliveide-vsext/src/custom-editors/disassembly/DisassemblyEntry.svelte
+++ packages/kliveide-vsext/src/custom-editors/disassembly/DisassemblyEntry.svelte
@@ -14,6 +14,7 @@
   let opCodesWidth = 0;
   let labelWidth = 0;
   let instructionWidth = 0;
+  let commentWidth = 0;
 
   const dispatch = createEventDispatcher();
 
@@ -21,39 +22,64 @@
     const charWidth = referenceWidth / 5;
     opCodesWidth = 14 * charWidth;
     labelWidth = 17 * charWidth;
-    instructionWidth = 65 * charWidth;
+    instructionWidth = 54 * charWidth;
+    commentWidth = 120 * charWidth;
   }
 </script>
 
 <style>
   .item {
     padding: 1px 8px;
+    height: 20px;
     display: flex;
     flex-direction: row;
     flex-grow: 0;
     flex-shrink: 0;
     font-family: Consolas, "Courier New", monospace;
     font-size: 1.1em;
-    overflow-x: hidden;
-    align-items: center;
+    overflow: hidden;
+    align-items: flex-start;
   }
+
+  .prefixComment {
+    width: 100%;
+    color: var(--vscode-terminal-ansiGreen);
+    flex-grow: 0;
+    flex-shrink: 0;
+  }
+
   .address {
     color: var(--vscode-editorLineNumber-foreground);
+    flex-grow: 0;
+    flex-shrink: 0;
   }
 
   .opCodes {
     color: var(--vscode-terminal-ansiBlue);
-    overflow-x: hidden;
+    overflow: hidden;
+    flex-grow: 0;
+    flex-shrink: 0;
   }
 
   .label {
     color: var(--vscode-terminal-ansiBrightMagenta);
-    overflow-x: hidden;
+    overflow: hidden;
+    flex-grow: 0;
+    flex-shrink: 0;
   }
 
   .instruction {
     color: var(--vscode-terminal-ansiBrightCyan);
-    overflow-x: hidden;
+    overflow: hidden;
+    flex-grow: 0;
+    flex-shrink: 0.25;
+  }
+
+  .comment {
+    color: var(--vscode-terminal-ansiBrightGreen);
+    overflow: hidden;
+    flex-grow: 1;
+    flex-shrink: 1;
   }
 
   .item:hover {
@@ -66,21 +92,35 @@
   }
 </style>
 
-<div class="item" class:currentBreakpoint={isCurrentBreakpoint} on:click={() => dispatch('clicked')}>
+<div
+  class="item"
+  class:currentBreakpoint={isCurrentBreakpoint}
+  on:click={() => dispatch('clicked')}>
   <BreakPointPlaceholder
     address={item.address}
     {hasBreakpoint}
     {isCurrentBreakpoint}
     {execState}
-    {runsInDebug} />
-  <span class="address" bind:clientWidth={referenceWidth}>
-    {intToX4(item.address)}&nbsp;
-  </span>
-  <span class="opCodes" style="width:{opCodesWidth}px">{item.opCodes}</span>
-  <span class="label" style="width:{labelWidth}px">
-    {item.hasLabel ? 'L' + intToX4(item.address) : ''}
-  </span>
-  <span class="instruction" style="width:{instructionWidth}px">
-    {item.instruction}
-  </span>
+    {runsInDebug}
+    isPrefix={item.isPrefixItem} />
+  {#if item.isPrefixItem}
+    <span class="prefixComment" title={item.prefixComment}>{item.prefixComment}</span>
+  {:else}
+    <span class="address" bind:clientWidth={referenceWidth}>
+      {intToX4(item.address)}&nbsp;
+    </span>
+    <span class="opCodes" style="width:{opCodesWidth}px">{item.opCodes}</span>
+    <span class="label" style="width:{labelWidth}px">
+      {item.formattedLabel}
+    </span>
+    <span class="instruction" style="width:{instructionWidth}px">
+      {item.instruction}
+    </span>
+    <div
+      class="comment"
+      style="width:{commentWidth}px"
+      title={item.formattedComment}>
+      {item.formattedComment}
+    </div>
+  {/if}
 </div>
--- packages/kliveide-vsext/src/custom-editors/disassembly/DisassemblyView.ts
+++ packages/kliveide-vsext/src/custom-editors/disassembly/DisassemblyView.ts
@@ -2,11 +2,13 @@ import {
   DisassemblyOutput,
   MemorySection,
   MemorySectionType,
+  DisassemblyItem,
 } from "../../disassembler/disassembly-helper";
 import { getMemoryContents } from "../messaging/messaging-core";
 import { Z80Disassembler } from "../../disassembler/z80-disassembler";
 import { DisassemblyAnnotation } from "../../disassembler/annotations";
 import { CancellationToken } from "../../utils/cancellation";
+import { intToX4 } from "../../disassembler/disassembly-helper";
 
 /**
  * Gets the disassembly for the specified memory range
@@ -19,11 +21,59 @@ export async function disassembly(
   annotations?: DisassemblyAnnotation | null,
   cancellation?: CancellationToken
 ): Promise<DisassemblyOutput | null> {
+  // --- Get the Z80 memory to disassemble
   const memoryContents = await getMemoryContents(from, to);
   const bytes = new Uint8Array(Buffer.from(memoryContents.bytes, "base64"));
+
+  // --- Use the memory sections in the annotations
   const sections: MemorySection[] = annotations?.memoryMap?.sections ?? [
     new MemorySection(from, to, MemorySectionType.Disassemble),
   ];
+
+  // --- Do the disassembly
   const disassembler = new Z80Disassembler(sections, bytes);
-  return await disassembler.disassemble(from, to, cancellation);
+  const rawItems = await disassembler.disassemble(from, to, cancellation);
+  if (!rawItems) {
+    return rawItems;
+  }
+
+  // --- Compose annotations
+  const updatedItems: DisassemblyItem[] = [];
+  for (const item of rawItems.outputItems) {
+    const prefixComment = annotations?.prefixComments.get(item.address);
+    if (prefixComment) {
+      const prefixItem = new DisassemblyItem(item.address);
+      prefixItem.isPrefixItem = true;
+      prefixItem.prefixComment = prefixComment;
+      updatedItems.push(prefixItem);
+    }
+    const formattedLabel = annotations?.labels.get(item.address);
+    item.formattedLabel =
+      formattedLabel ?? (item.hasLabel ? "L" + intToX4(item.address) : "");
+    item.formattedComment = item.hardComment ? item.hardComment + " " : "";
+    const comment = annotations?.comments.get(item.address);
+    if (comment) {
+      item.formattedComment += comment;
+    }
+    if (annotations && item.tokenLength > 0) {
+      let symbol: string | undefined;
+      if (item.hasLabelSymbol) {
+        const label = annotations.labels.get(item.symbolValue);
+        if (label) {
+          symbol = label;
+        }
+      } else {
+        symbol = annotations.literalReplacements.get(item.address);
+      }
+      if (symbol && item.instruction) {
+        item.instruction =
+          item.instruction.substr(0, item.tokenPosition) +
+          symbol +
+          item.instruction.substr(item.tokenPosition + item.tokenLength);
+      }
+    }
+    updatedItems.push(item);
+  }
+  rawItems.replaceOutputItems(updatedItems);
+  return rawItems;
 }
