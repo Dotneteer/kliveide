00e5d42d4777c2fd42ef4262bded3485bc34d699
Istvan Novak <dotneteer@hotmail.com>
2020-08-19 17:34:20 +0200

    feat: allow configuring the port that Klive Emulator uses for communication

--- packages/kliveide-emu/src/remote-api/api-server.ts
+++ packages/kliveide-emu/src/remote-api/api-server.ts
@@ -28,7 +28,8 @@ import { breakpointEraseAllAction } from "../shared/state/redux-breakpoint-state
 import { checkTapeFile } from "../shared/tape/readers";
 import { BinaryReader } from "../shared/utils/BinaryReader";
 import { IdeConfiguration } from "../shared/state/AppState";
-import { ideConfigSetAction, ideConfigStateReducer } from "../shared/state/redux-ide-config-state";
+import { ideConfigSetAction} from "../shared/state/redux-ide-config-state";
+import { appConfiguration } from "../main/klive-configuration";
 
 /**
  * Starts the web server that provides an API to manage the Klive emulator
@@ -378,5 +379,7 @@ export function startApiServer() {
     res.sendStatus(204);
   });
 
-  app.listen(3000, () => console.log("Klive Emulator is listening on port 3000..."));
+  // --- Start the API server on the configured port
+  const port = appConfiguration?.port ?? 3000;
+  app.listen(port, () => console.log(`Klive Emulator is listening on port ${port}...`));
 }
