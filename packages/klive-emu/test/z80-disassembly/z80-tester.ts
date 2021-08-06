d0ea8ae2ce76a83b0c4552772da44a03bedd64e2
Dotneteer <dotneteer@hotmail.com>
2021-08-06 18:13:19 +0200

    wip

---
+++ packages/klive-emu/test/z80-disassembly/z80-tester.ts
@@ -0,0 +1,80 @@
+import * as expect from "expect";
+import { intToX2, MemoryMap, MemorySection} from "../../src/shared/z80/disassembler/disassembly-helper"
+import { Z80Disassembler } from "../../src/shared/z80/disassembler/z80-disassembler";
+
+/**
+ * Helper class for Z80 Disassembler testing
+ */
+export class Z80Tester {
+  /**
+   * Tests if Z80 instruction disassembly works
+   * @param expected Expected disassembly text
+   * @param opCodes Operation codes
+   */
+  static async Test(expected: string, ...opCodes: number[]): Promise<void> {
+    const map = new MemoryMap();
+    map.add(new MemorySection(0x0000, opCodes.length - 1));
+
+    const disassembler = new Z80Disassembler(
+      map.sections,
+      new Uint8Array(opCodes)
+    );
+    var output = await disassembler.disassemble();
+    expect(output).not.toBeNull();
+    if (output === null) {
+      return;
+    }
+    expect(output.outputItems.length).toBe(1);
+    const item = output.outputItems[0];
+    expect(item.instruction).toBeTruthy();
+    if (!item.instruction) {
+      return;
+    }
+    expect(item.instruction.toLowerCase()).toBe(expected.toLowerCase());
+    expect(item.opCodes ? item.opCodes.trim() : "").toBe(this._joinOpCodes(opCodes));
+  }
+
+  /**
+   * Tests if Z80 extended instruction set disassembly works
+   * @param expected Expected disassembly text
+   * @param opCodes Operation codes
+   */
+  static async TestExt(expected: string, ...opCodes: number[]): Promise<void> {
+    const map = new MemoryMap();
+    map.add(new MemorySection(0x0000, opCodes.length - 1));
+
+    const disassembler = new Z80Disassembler(
+      map.sections,
+      new Uint8Array(opCodes),
+      true
+    );
+    var output = await disassembler.disassemble();
+    expect(output).not.toBeNull();
+    if (output === null) {
+      return;
+    }
+    expect(output.outputItems.length).toBe(1);
+    const item = output.outputItems[0];
+    expect(item.instruction).toBeTruthy();
+    if (!item.instruction) {
+      return;
+    }
+    expect(item.instruction.toLowerCase()).toBe(expected.toLowerCase());
+    expect(item.opCodes ? item.opCodes.trim() : "").toBe(this._joinOpCodes(opCodes));
+  }
+
+  /**
+   * Joins the opcodes into a string
+   * @param opCodes Opecration codes
+   */
+  private static _joinOpCodes(opCodes: number[]): string {
+    let result = "";
+    for (let i = 0; i < opCodes.length; i++) {
+      if (i > 0) {
+        result += " ";
+      }
+      result += intToX2(opCodes[i]);
+    }
+    return result;
+  }
+}
