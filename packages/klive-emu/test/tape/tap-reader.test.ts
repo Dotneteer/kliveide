167ac1d52ee2b6841255c3f8718dffc618cac32e
Istvan Novak <dotneteer@hotmail.com>
2021-10-14 07:52:22 +0200

    wip

--- packages/klive-emu/test/tape/tap-reader.test.ts
+++ packages/klive-emu/test/tape/tap-reader.test.ts
@@ -3,8 +3,8 @@ import * as fs from "fs";
 import * as path from "path";
 import * as expect from "expect";
 
-import { BinaryReader } from "../../src/shared/utils/BinaryReader";
-import { TapReader } from "../../src/shared/tape/tap-file";
+import { BinaryReader } from "../../src/core/utils/BinaryReader";
+import { TapReader } from "../../src/renderer/machines/zx-spectrum/tap-file";
 
 describe("Tape - TAP Reader", () => {
   it("Can read TZX file", () => {
