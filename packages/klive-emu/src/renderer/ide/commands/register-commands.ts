0896c3b6220e31cea8fdd66fc1ceb0b5d0b4af15
Istvan Novak <dotneteer@hotmail.com>
2021-10-14 13:57:28 +0200

    feat: allow setting and removing breakpoints from the Interactive pane

--- packages/klive-emu/src/renderer/ide/commands/register-commands.ts
+++ packages/klive-emu/src/renderer/ide/commands/register-commands.ts
@@ -1,9 +1,17 @@
 import { getCommandService } from "@core/service-registry";
+import { AddBreakpointCommand } from "./AddBreakpointCommand";
+import { ClearBreakpointsCommand } from "./ClearBreakpointsCommand";
+import { ListBreakpointsCommand } from "./ListBreakpointsCommand";
 import { NewProjectCommand } from "./NewProjectCommand";
+import { RemoveBreakpointCommand } from "./RemoveBreakpointCommand";
 
 /**
  * This function registers all Klive commands used in the IDE
  */
 export function registerKliveCommands(): void {
   getCommandService().registerCommand(new NewProjectCommand());
+  getCommandService().registerCommand(new ListBreakpointsCommand());
+  getCommandService().registerCommand(new AddBreakpointCommand());
+  getCommandService().registerCommand(new RemoveBreakpointCommand());
+  getCommandService().registerCommand(new ClearBreakpointsCommand());
 }
