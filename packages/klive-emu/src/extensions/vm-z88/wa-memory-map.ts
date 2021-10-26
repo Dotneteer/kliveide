5e3191e351bdcac415e0f1a1ca27621ee82a3625
Istvan Novak <dotneteer@hotmail.com>
2021-10-26 17:18:08 +0200

    refactor: create WAT# aliases to separate machine-independent and machine-specific memory segments

--- packages/klive-emu/src/extensions/vm-z88/wa-memory-map.ts
+++ packages/klive-emu/src/extensions/vm-z88/wa-memory-map.ts
@@ -1,11 +1,8 @@
 // ============================================================================
 // Cambridge Z88 specific memory map
 
-// --- Machine state trasnfer buffer
-export const Z88_MACHINE_STATE_BUFFER = 0x016c30f0;
-
 // --- Z88 pixel buffer area
-export const Z88_PIXEL_BUFFER = 0x010e30f0;
+export const Z88_PIXEL_BUFFER = 0x010e_4af4;
 
 // --- Z88 beeper buffer area
-export const Z88_BEEPER_BUFFER = 0x010e10f0;
+export const Z88_BEEPER_BUFFER = 0x010e_2af4;
--- packages/klive-emu/src/extensions/vm-zx-spectrum/wa-memory-map.ts
+++ packages/klive-emu/src/extensions/vm-zx-spectrum/wa-memory-map.ts
@@ -2,7 +2,7 @@
 // ZX Spectrum specific memory map
 
 // --- The beginning of the rendering tact table
-export const RENDERING_TACT_TABLE = 0x010e0fe4;
+export const RENDERING_TACT_TABLE = 0x010e_29e8;
 
 // --- Offset address for bank 0
 export const BANK_0_OFFSET = 0x00_8000;
@@ -14,20 +14,19 @@ export const ROM_48_OFFSET = 0x00_4000;
 export const ROM_128_0_OFFSET = 0x00_0000;
 
 // --- Pixel rendering buffer
-export const PIXEL_RENDERING_BUFFER = 0x012c0fe4;
+export const PIXEL_RENDERING_BUFFER = 0x012c_29e8;
 
 // --- Colorization buffer
-export const COLORIZATION_BUFFER = 0x012e8fe4;
+export const COLORIZATION_BUFFER = 0x012e_a9e8;
 
 // --- Beeper samples
-export const BEEPER_SAMPLE_BUFFER = 0x013d9434;
+export const BEEPER_SAMPLE_BUFFER = 0x013d_ae38;
 
 // --- AY PSG chip samples
-export const PSG_SAMPLE_BUFFER = 0x013efd3f;
+export const PSG_SAMPLE_BUFFER = 0x0101_610b;
 
 // --- Tape LOAD buffer
-export const TAPE_DATA_BUFFER = 0x0138942c;
+export const TAPE_DATA_BUFFER = 0x0138_ae30;
 
 // --- Tape SAVE buffer
-export const TAPE_SAVE_BUFFER = 0x013c942c;
-
+export const TAPE_SAVE_BUFFER = 0x013c_ae30;
