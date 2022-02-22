ef0067ee7f948d8220112f570f48bfab33c92b1f
Istvan Novak <dotneteer@hotmail.com>
2022-02-22 17:44:00 +0100

    fix: use full execution path when running a process on MacOS

--- src/main/compiler-integration/CompilerBase.ts
+++ src/main/compiler-integration/CompilerBase.ts
@@ -6,6 +6,7 @@ import {
   KliveCompilerOutput,
 } from "@abstractions/compiler-registry";
 import { AssemblerErrorInfo } from "@abstractions/z80-compiler-service";
+import { __DARWIN__, __WIN32__ } from "../utils/electron-utils";
 
 /**
  * Helper class to invoke compilers and communicate with the IDE
@@ -61,12 +62,12 @@ export abstract class CompilerBase implements IKliveCompiler {
   async executeCommandLine(
     execPath: string,
     cmdArgs: string,
-    options?: any
+    _options?: any
   ): Promise<(AssemblerErrorInfo | string)[] | null> {
     const workdir = path.dirname(execPath);
     const filename = path.basename(execPath);
-
-    const cmd = `${filename} ${cmdArgs.split("\\").join("/")}`;
+    const args = cmdArgs.split("\\").join("/")
+    const cmd = `${__DARWIN__ ? execPath : filename} ${args}`;
     return new Promise<(AssemblerErrorInfo | string)[] | null>(
       (resolve, reject) => {
         const process = exec(
@@ -75,7 +76,7 @@ export abstract class CompilerBase implements IKliveCompiler {
             cwd: workdir,
           },
           (error, _stdout, stderr) => {
-            const processedMessages = this.processErrorString(stderr)
+            const processedMessages = this.processErrorString(stderr);
             if (error || processedMessages.length > 0) {
               resolve(processedMessages);
               return;
