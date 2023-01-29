50874fd38790e106de2bc97eeb116e6684ceb5b1
dotneteer <dotneteer@hotmail.com>
2023-01-29 07:07:11 +0100

    wip: SplitPanel initial sizing implemented

--- src/appEmu/EmulatorArea/EmulatorArea.tsx
+++ src/appEmu/EmulatorArea/EmulatorArea.tsx
@@ -5,18 +5,19 @@ import styles from "./EmulatorArea.module.scss";
 import { useSelector } from "@/core/RendererProvider";
 
 export const EmulatorArea = () => {
-    const keyboardVisible = useSelector(s => s.emuViewOptions?.showKeyboard ?? false);
-    return  (
-    <div className={styles.component} >
-        <SplitPanel
-            id="emulator"
-            primaryLocation="top"
-            primaryPanel={<EmulatorPanel />}
-            initialPrimarySize="50%"
-            secondaryPanel={<KeyboardPanel />}
-            secondaryVisible={keyboardVisible}
-            minSize={40}
-        />
+  const keyboardVisible = useSelector(
+    s => s.emuViewOptions?.showKeyboard ?? false
+  );
+  return (
+    <div className={styles.component}>
+      <SplitPanel
+        primaryLocation='top'
+        primaryPanel={<EmulatorPanel />}
+        initialSecondarySize="33%"
+        secondaryPanel={<KeyboardPanel />}
+        secondaryVisible={keyboardVisible}
+        minSize={40}
+      />
     </div>
-    )
-}
+  );
+};
