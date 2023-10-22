cf18db8ca29d9c95e0e9c43fa14ab052d53e1c8f
Istvan Novak <dotneteer@hotmail.com>
2023-10-22 07:18:50 +0200

    feat: save machine type changes to project (#590)

---
+++ src/main/registeredMachines.ts
@@ -0,0 +1,77 @@
+import { sendFromMainToEmu } from "../common/messaging/MainToEmuMessenger";
+import { sendFromMainToIde } from "../common/messaging/MainToIdeMessenger";
+import { OutputColor } from "@renderer/appIde/ToolArea/abstractions";
+
+export const registeredMachines = [
+  {
+    id: "sp48",
+    displayName: "ZX Spectrum 48K"
+  },
+  {
+    id: "sp128",
+    displayName: "ZX Spectrum 128K"
+  },
+  {
+    id: "spp2e",
+    displayName: "ZX Spectrum +2E"
+  },
+  {
+    id: "spp3e",
+    displayName: "ZX Spectrum +3E (1 FDD)",
+    supportedFdds: 1
+  },
+  {
+    id: "spp3ef2",
+    displayName: "ZX Spectrum +3E (2 FDDs)",
+    supportedFdds: 2
+  }
+];
+
+/**
+ * This function set the machine type to the specified one
+ * @param machineId
+ */
+export async function setMachineType (machineId: string): Promise<void> {
+  await sendFromMainToEmu({
+    type: "EmuSetMachineType",
+    machineId
+  });
+  const mt = registeredMachines.find(mt => mt.id === machineId);
+  if (mt) {
+    await logEmuEvent(
+      `Machine type set to ${mt.displayName} (${mt.id})`,
+      "bright-cyan"
+    );
+  }
+}
+
+// --- The number of events logged with the emulator
+let loggedEmuOutputEvents = 0;
+
+/**
+ * Log emulator events
+ * @param text Log text
+ * @param color Text color to use
+ */
+export async function logEmuEvent (
+    text: string,
+    color?: OutputColor
+  ): Promise<void> {
+    loggedEmuOutputEvents++;
+    await sendFromMainToIde({
+      type: "IdeDisplayOutput",
+      pane: "emu",
+      text: `[${loggedEmuOutputEvents}] `,
+      color: "yellow",
+      writeLine: false
+    });
+    await sendFromMainToIde({
+      type: "IdeDisplayOutput",
+      pane: "emu",
+      text,
+      color,
+      writeLine: true
+    });
+  }
+  
+  
