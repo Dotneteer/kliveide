b2b92c9dbeb3e048fcd491a88f5e63e653df80a2
Gunther Strube <gstrube@gmail.com>
2024-02-25 20:43:26 +0100

    Added test for device + manufacturer codes of Intel i28F008S5

--- test/z88/memory-intflash-io.test.ts
+++ test/z88/memory-intflash-io.test.ts
@@ -13,7 +13,7 @@ const addrSR3: number[] = [
 
 describe("Z88 - Intel I28F00XS5 Card Read / flash bytes", function () {
   addrSR3.forEach(addr => {
-    it(`Intel I28F00XS5 read pristine content (${addr}) in slot 3`, () => {
+    it(`Intel i28F004S5 read pristine content (${addr}) in slot 3`, () => {
       // --- Create the machine
       const m = new Z88TestMachine();
       const mem = m.memory;
@@ -43,7 +43,37 @@ describe("Z88 - Intel I28F00XS5 Card Read / flash bytes", function () {
   });
 
   addrSR3.forEach(addr => {
-    it(`Intel I28F00XS5 read pristine content (${addr}) in slot 2`, () => {
+    it(`Intel i28F008S5 read pristine content (${addr}) in slot 3`, () => {
+      // --- Create the machine
+      const m = new Z88TestMachine();
+      const mem = m.memory;
+      const memt = mem as IZ88BankedMemoryTestSupport;
+
+      // --- Create a 1Mb Intel 28F008S5 Flash Card
+      const i28F008S5 = new Z88IntelFlashMemoryCard(m, 0x10_0000);
+      // --- Insert 1Mb card in slot 3 (reset to FFh)
+      mem.insertCard(3, i28F008S5);
+
+      // bind bottom bank of slot 3 into logical address space (SR3)
+      m.blinkDevice.setSR3(0xc0);
+
+      const value = m.memory.readMemory(addr);
+      expect(value).toBe(0xff);
+
+      // Get Manufacturer & Device code from chip
+      m.memory.writeMemory(0xc800,0x90); // any (address) write to slot, execute Device ID command
+      expect(i28F008S5.readArrayModeState()).toBe(false);
+      const manCode= m.memory.readMemory(0xc000); // command mode: read Manufacturer Code from address 0x0000
+      const devCode= m.memory.readMemory(0xc001); // command mode: read Device Code from address 0x0001
+      expect(manCode).toBe(CardType.FlashIntel28F008S5 >> 8);
+      expect(devCode).toBe(CardType.FlashIntel28F008S5 & 0xff);
+      m.memory.writeMemory(addr,0xff); // any (address) write to slot, Back to Read Array Mode command
+      expect(i28F008S5.readArrayModeState()).toBe(true);
+    });
+  });
+
+  addrSR3.forEach(addr => {
+    it(`Intel i28F004S5 read pristine content (${addr}) in slot 2`, () => {
       // --- Create the machine
       const m = new Z88TestMachine();
       const mem = m.memory;
@@ -67,7 +97,7 @@ describe("Z88 - Intel I28F00XS5 Card Read / flash bytes", function () {
       const devCode= m.memory.readMemory(0xc001); // read Device Code from address 0x0001
       expect(manCode).toBe(CardType.FlashIntel28F004S5 >> 8);
       expect(devCode).toBe(CardType.FlashIntel28F004S5 & 0xff);
-      m.memory.writeMemory(0xc8ff,0xff); // any (address) write to slot, Back to Read Array Mode command
+      m.memory.writeMemory(addr,0xff); // any (address) write to slot, Back to Read Array Mode command
       expect(i28F004S5.readArrayModeState()).toBe(true);
     });
   });
