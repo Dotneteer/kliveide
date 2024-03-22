8cfe053f30f06bee0a7667f4427c5934ca92471b
Gunther Strube <gstrube@gmail.com>
2024-03-22 08:44:24 +0100

    porting from Java, w.i.p...

--- src/emu/machines/z88/memory/Z88AmdFlashMemoryCard.ts
+++ src/emu/machines/z88/memory/Z88AmdFlashMemoryCard.ts
@@ -15,22 +15,19 @@ import { IZ88Machine } from "@renderer/abstractions/IZ88Machine";
 import { CardType } from "@emu/machines/z88/memory/CardType";
 import { Z88MemoryCardBase } from "./Z88MemoryCardBase";
 
-
 class Stack<T> {
   private _store: T[] = [];
 
-  push(val: T) {
+  push (val: T) {
     this._store.push(val);
   }
 
-  pop(): T | undefined {
+  pop (): T | undefined {
     return this._store.pop();
   }
 }
 
-
 export class Z88AmdFlashMemoryCard extends Z88MemoryCardBase {
-
   /**
    * The AMD Flash card type is 16bits and contains Manufacturer and Device code
    * (upper 8 bits is Manufacturer code, lower 8 bits Device code)
@@ -49,7 +46,7 @@ export class Z88AmdFlashMemoryCard extends Z88MemoryCardBase {
    * by the third cycle which is the command ('?') code (the actual command
    * will then be verified against known codes).
    */
-  private readonly commandUnlockCycles: number[] = [0x555, 0xAA, 0x2AA, 0x55, 0x555, Number('?')];
+  private readonly commandUnlockCycles: number[] = [0x555,0xaa,0x2aa,0x55,0x555,Number("?")];
 
   /**
    * Indicate success by DQ5 = 0 and DQ6 = 1, signaling no toggle in two
@@ -134,14 +131,143 @@ export class Z88AmdFlashMemoryCard extends Z88MemoryCardBase {
   private executingCommandCode: number;
 
   /**
+   * Prepare a sequence stack.
+   *
+   * This will be used for validating a complete 3 cycle command sequence or
+   * when an application polls for chip command status.
+   */
+  private presetSequence (sequence: number[]): Stack<Number> {
+    var seqStk = new Stack<Number>();
+
+    // prepare a new sequence
+    for (var p: number = sequence.length - 1; p >= 0; p--) {
+      seqStk.push(sequence[p]);
+    }
+
+    return seqStk;
+  }
+
+  /**
+   * Process each command cycle sent to the Command Decoder, and execute the
+   * parsed command, once it has been identified. If a command cycle is not
+   * recognized as being a part of a command (address/data) sequence, the chip
+   * automatically returns to Read Array Mode. Equally, if a read cycle is
+   * performed against the Command Decoder while it is expecting a command
+   * write cycle, the chip automatically returns to Read Array Mode.
+   *
+   * @param addr
+   * @param b
+   */
+  private processCommandCycle (addr: number, b: number) {
+    if (this.readArrayMode == true) {
+      // get into command mode...
+      this.readArrayMode = false;
+      this.isCommandAccumulating = true;
+      this.executingCommandCode = 0;
+      this.commandUnlockCycleStack = this.presetSequence(this.commandUnlockCycles);
+    }
+
+    if (this.isCommandAccumulating == false) {
+      if (b == 0xf0) {
+        // Reset to Read Array Mode; abort the error command mode state.
+        // This command will be executed immediately, unless the Flash Memory has begun
+        // programming or erasing (Read Array Mode command will then be ignored).
+
+        this.readArrayMode = true;
+        this.isCommandAccumulating = false;
+        this.isCommandExecuting = false;
+      }
+
+      return;
+    }
+
+    // only accept other write cycles while accumulating a command (it's probably part of the command!)
+    var cmdAddr = this.commandUnlockCycleStack.pop(); // validate cycle against this address
+    var cmd = this.commandUnlockCycleStack.pop(); // validate cycle against this data
+
+    if (cmd != Number("?")) {
+      // gathering the unlock cycles...
+      addr &= 0x07ff; // we're only interested in bits 0-10 in the unlock cycle address...
+      if (addr != cmdAddr || b != cmd) {
+        // command sequence was 0xF0 (back to Read Array Mode) or an unknown command!
+        // Immediately return to Read Array Mode...
+        this.readArrayMode = true;
+        this.isCommandAccumulating = false;
+        this.isCommandExecuting = false;
+      }
+
+      return;
+    }
+
+    // we're reached the actual command code (Top of Stack)!
+    if (this.executingCommandCode == 0xa0) {
+      // Byte Program Command, Part 2, we've fetched the Byte Program Address & Data,
+      // programming will now begin...
+      this.isCommandAccumulating = false;
+      this.isCommandExecuting = true;
+      // this.programByteAtAddress(addr, b);
+    } else {
+      switch (b) {
+        case 0x10:
+          // Chip Erase Command, part 2
+          this.isCommandAccumulating = false;
+          this.isCommandExecuting = true;
+          this.executingCommandCode = 0x10;
+          // this.eraseChip();
+          break;
+
+        case 0x30:
+          // Sector Erase Command (which this bank is part of), part 2
+          this.isCommandAccumulating = false;
+          this.isCommandExecuting = true;
+          this.executingCommandCode = 0x30;
+          // this.eraseSector(addr);
+          break;
+
+        case 0x80: // Erase Command, part 1
+          // add new sub command sequence for erase chip or sector
+          // and wait for application command cycles...
+          this.commandUnlockCycleStack = this.presetSequence(
+            this.commandUnlockCycles
+          );
+          break;
+
+        case 0x90:
+          // Autoselect Command (get Manufacturer & Device Code)
+          this.isCommandAccumulating = false;
+          this.isCommandExecuting = true;
+          this.executingCommandCode = 0x90;
+          break;
+
+        case 0xa0:
+          // Byte Program Command, Part 1, get address and byte to program..
+          this.executingCommandCode = 0xa0;
+          this.commandUnlockCycleStack.push(Number("?")); // and the Byte Program Data
+          this.commandUnlockCycleStack.push(Number("?")); // We still need the Byte Program Address
+          break;
+
+        default:
+          // command cycle sequence was unknown!
+          // Immediately return to Read Array Mode...
+          this.readArrayMode = true;
+          this.isCommandAccumulating = false;
+          this.isCommandExecuting = false;
+      }
+    }
+  }
+
+  /**
    * Initializes the AMD Flash card with the specified size (512K or 1024K)
    * Type is defined by derived class.
    */
-  public constructor (public readonly host: IZ88Machine, public readonly size: number) {
+  public constructor (
+    public readonly host: IZ88Machine,
+    public readonly size: number
+  ) {
     super(host, size);
 
     this.readArrayMode = true;
-    this.type = CardType.None; // base class, type is defined in specific Amd chip
+    this.type = CardType.None; // base class, type is defined in specific Amd chip class
   }
 
   /**
@@ -149,7 +275,7 @@ export class Z88AmdFlashMemoryCard extends Z88MemoryCardBase {
    *
    * @returns true if the AMD Flash chip is in Read Array Mode, false when in command mode
    */
-  public readArrayModeState(): boolean {
+  public readArrayModeState (): boolean {
     return this.readArrayMode;
   }
 
@@ -164,7 +290,7 @@ export class Z88AmdFlashMemoryCard extends Z88MemoryCardBase {
   public readMemory (memOffset: number, bank: number, address: number): number {
     const _22bitaddr = memOffset + (address & 0x1fff);
 
-    // Read behaviour of a Intel Flash (when in Read Array mode) is just like EPROM...
+    // Read behaviour of an AMD Flash (when in Read Array mode) is just like EPROM...
     if (this.readArrayMode == true) {
       return this.host.memory.memory[_22bitaddr];
     } else {
@@ -186,7 +312,7 @@ export class Z88AmdFlashMemoryCard extends Z88MemoryCardBase {
     memOffset: number,
     bank: number, // the bank of current address
     address: number, // the 64K logical address from Z80
-    byte: number // the byte to blow to Intel Flash card
+    byte: number // the byte to blow to AMD Flash card
   ): void {
     // deliver 16bit -> 22bit address of Intel Flash Card
     // this.processCommand(bank, memOffset + (address & 0x1fff), byte);
