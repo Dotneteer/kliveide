614c356477fb1658ea9f2d3518fdcd0103f41415
Istvan Novak <dotneteer@hotmail.com>
2024-03-25 17:57:22 +0100

    feat: allow starting/stopping scripts from the document command bar (#736)

--- src/renderer/appIde/DocumentArea/ScriptingCommandBar.tsx
+++ src/renderer/appIde/DocumentArea/ScriptingCommandBar.tsx
@@ -6,26 +6,57 @@ import { AppState } from "@common/state/AppState";
 import { Store } from "@common/state/redux-light";
 import { isScriptCompleted } from "@common/utils/script-utils";
 import { AppServices } from "@renderer/abstractions/AppServices";
+import { useSelector } from "@renderer/core/RendererProvider";
+import { useEffect, useState } from "react";
+
+type Props = {
+  path: string;
+};
 
 /**
  * Represents the command bar for files that support scripting.
  */
-const ScriptingCommandBar = () => {
+const ScriptingCommandBar = ({ path }: Props) => {
+  console.log("ScriptingCommandBar", path);
   const { outputPaneService, ideCommandsService } = useAppServices();
+  const scriptsInfo = useSelector(s => s.scripts);
+  const [scriptRunning, setScriptRunning] = useState(false);
+
+  useEffect(() => {
+    const scripts = scriptsInfo.slice().reverse();
+    const script = scripts.find(
+      s => s.scriptFileName === path && !isScriptCompleted(s.status)
+    );
+    setScriptRunning(!!script);
+  }, [scriptsInfo]);
+
   return (
     <>
       <TabButtonSeparator />
-      <TabButton
-        iconName='play'
-        title='Run this script file'
-        clicked={async () => {
-          const scriptPane =
-            outputPaneService.getOutputPaneBuffer(PANE_ID_SCRIPTIMG);
-          scriptPane.writeLine("Running script...");
-          scriptPane.writeLine("Script completed.");
-          await ideCommandsService.executeCommand(`outp ${PANE_ID_SCRIPTIMG}`);
-        }}
-      />
+      {!scriptRunning && (
+        <TabButton
+          iconName='play'
+          title='Run this script file'
+          clicked={async () => {
+            await ideCommandsService.executeCommand(
+              `outp ${PANE_ID_SCRIPTIMG}`
+            );
+            await ideCommandsService.executeCommand(`script-run "${path}"`);
+          }}
+        />
+      )}
+      {scriptRunning && (
+        <TabButton
+          iconName='stop'
+          title='Stop this script file'
+          clicked={async () => {
+            await ideCommandsService.executeCommand(
+              `outp ${PANE_ID_SCRIPTIMG}`
+            );
+            await ideCommandsService.executeCommand(`script-cancel "${path}"`);
+          }}
+        />
+      )}
     </>
   );
 };
@@ -33,6 +64,7 @@ const ScriptingCommandBar = () => {
 export function getScriptingContextMenuIfo (
   services: AppServices
 ): ContextMenuInfo[] {
+  const { ideCommandsService } = services;
   return [
     {
       text: "Run script",
@@ -45,7 +77,8 @@ export function getScriptingContextMenuIfo (
         return !!script;
       },
       clicked: async (item: string) => {
-        services.ideCommandsService.executeCommand(`script-run "${item}"`);
+        await ideCommandsService.executeCommand(`outp ${PANE_ID_SCRIPTIMG}`);
+        await ideCommandsService.executeCommand(`script-run "${item}"`);
       }
     },
     {
@@ -59,10 +92,13 @@ export function getScriptingContextMenuIfo (
         return !script;
       },
       clicked: async (item: string) => {
-        services.ideCommandsService.executeCommand(`script-cancel "${item}"`);
+        await ideCommandsService.executeCommand(`outp ${PANE_ID_SCRIPTIMG}`);
+        await ideCommandsService.executeCommand(`script-cancel "${item}"`);
       }
     }
   ];
 }
 
-export const scriptingCommandBarRenderer = () => <ScriptingCommandBar />;
+export const scriptingCommandBarRenderer = (path: string) => (
+  <ScriptingCommandBar path={path} />
+);
