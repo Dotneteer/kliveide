926547cf1b939b8d4a493aeb67c2fc4d298402e3
Istvan Novak <dotneteer@hotmail.com>
2024-04-13 10:07:20 +0200

    feat: implement ZX Spectrum +2/3 floating bus (#752)

--- src/emu/machines/zxSpectrumP3e/ZxSpectrumP3eFloatingBusDevice.ts
+++ src/emu/machines/zxSpectrumP3e/ZxSpectrumP3eFloatingBusDevice.ts
@@ -1,6 +1,7 @@
 import { IFloatingBusDevice } from "@emu/abstractions/IFloatingBusDevice";
 import { IZxSpectrumMachine } from "@renderer/abstractions/IZxSpectrumMachine";
 import { RenderingPhase } from "@renderer/abstractions/RenderingPhase";
+import { ZxSpectrumP3EMachine } from "./ZxSpectrumP3eMachine";
 
 /**
  * This class implements the ZX Spectrum 128 floating bus device.
@@ -31,22 +32,28 @@ export class ZxSpectrumP3eFloatingBusDevice implements IFloatingBusDevice {
    * @returns The value read from the floating bus
    */
   public readFloatingBus (): number {
+    const machine = (this.machine as ZxSpectrumP3EMachine);
     const screen = this.machine.screenDevice;
     const currentTactIndex =
       (this.machine.currentFrameTact - 3 + this.machine.tactsInFrame) %
       this.machine.tactsInFrame;
     const renderingTact = screen.renderingTactTable[currentTactIndex];
     switch (renderingTact?.phase) {
-      case RenderingPhase.BorderFetchPixel:
-      case RenderingPhase.DisplayB1FetchB2:
-      case RenderingPhase.DisplayB2FetchB1:
-        return this.machine.readScreenMemory(renderingTact.pixelAddress);
-      case RenderingPhase.BorderFetchAttr:
-      case RenderingPhase.DisplayB1FetchA2:
-      case RenderingPhase.DisplayB2FetchA1:
-        return this.machine.readScreenMemory(renderingTact.attributeAddress);
+      case RenderingPhase.Border:
+      case RenderingPhase.None:
+      case RenderingPhase.DisplayB1:  
+      case RenderingPhase.DisplayB2:
+        return machine.lastContendedValue | 0x01;
       default:
-        return 0xff;
+        return machine.lastUlaReadValue;
     }
   }
+}
+
+/**
+ * Floating bus ports for the ZX Spectrum +2/+3 machine
+ */
+export const zxSpectrumP32FloatingBusPorts: Record<string, boolean> = {};
+for (let n = 0; n < 0x1000; n++) {
+  zxSpectrumP32FloatingBusPorts[4 * n + 1] = true;
 }
