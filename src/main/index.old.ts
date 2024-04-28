de25fd8346e6213b1120932b39b2755b3543405d
Istvan Novak <dotneteer@hotmail.com>
2024-04-28 07:06:26 +0200

    wip: electron-vite started

---
+++ src/main/index.old.ts
@@ -0,0 +1,569 @@
+// ====================================================================================================================
+// This file contains the startup code of the Klive application.
+//
+// This code creates two renderer processes with their corresponding windows:
+// - `emuWindow`: Displays the emulator
+// - `ideWindow`: Displays the IDE tools
+//
+// By default, only the EMU window is displayed. The app displays the IDE window whenever the user requests directly
+// (with the Show IDE function) or indirectly (with any other menu commands that require the IDE).
+//
+// The app manages the communication among the three processes (main, emu, ide). The application state is synched;
+// thus, if any of these processes change the state, the actions causing the changes are sent to the other two
+// processes.
+//
+// Both renderer processes load the same `index.html` file into their browsers. However, they add a parameter to let
+// the renderer process know whether it functions as an emulator (`?emu` parameter) or as an IDE window (`?ide`
+// parameter).
+// ====================================================================================================================
+
+import * as fs from "fs";
+import {
+  defaultResponse,
+  errorResponse,
+  RequestMessage,
+  ResponseMessage
+} from "../common/messaging/messages-core";
+import {
+  emuFocusedAction,
+  ideFocusedAction,
+  saveUserSettingAction,
+  isWindowsAction,
+  unloadWindowsAction,
+  setClockMultiplierAction,
+  setSoundLevelAction,
+  setThemeAction,
+  showKeyboardAction,
+  setFastLoadAction,
+  displayDialogAction,
+  startScreenDisplayedAction,
+  setKeyMappingsAction,
+  showEmuToolbarAction,
+  showEmuStatusBarAction,
+  showIdeToolbarAction,
+  showIdeStatusBarAction,
+  primaryBarOnRightAction,
+  toolPanelsOnTopAction,
+  maximizeToolsAction,
+  emuSetKeyboardLayoutAction,
+  setMachineSpecificAction
+} from "../common/state/actions";
+import { Unsubscribe } from "../common/state/redux-light";
+import { app, BrowserWindow, shell, ipcMain, Menu } from "electron";
+import { release } from "os";
+import { join } from "path";
+import { setupMenu } from "./app-menu";
+import { __WIN32__ } from "../electron/electron-utils";
+import { processRendererToMainMessages } from "./RendererToMainProcessor";
+import { mainStore } from "./main-store";
+import { registerMainToEmuMessenger } from "../common/messaging/MainToEmuMessenger";
+import {
+  registerMainToIdeMessenger,
+  sendFromMainToIde
+} from "../common/messaging/MainToIdeMessenger";
+import { appSettings, loadAppSettings, saveAppSettings } from "./settings";
+import { createWindowStateManager } from "./WindowStateManager";
+import { registerCompiler } from "./compiler-integration/compiler-registry";
+import { Z80Compiler } from "./z80-compiler/Z80Compiler";
+import { setMachineType } from "./registeredMachines";
+import { ZxBasicCompiler } from "./zxb-integration/ZxBasicCompiler";
+import { createSettingsReader } from "../common/utils/SettingsReader";
+import { FIRST_STARTUP_DIALOG_EMU } from "../common/messaging/dialog-ids";
+import { parseKeyMappings } from "./key-mappings/keymapping-parser";
+import { setSelectedTapeFile } from "./machine-menus/zx-specrum-menus";
+import { MEDIA_TAPE } from "../common/structs/project-const";
+import { processBuildFile } from "./build";
+
+// --- We use the same index.html file for the EMU and IDE renderers. The UI receives a parameter to
+// --- determine which UI to display
+const EMU_QP = "?emu"; // EMU discriminator
+const IDE_QP = "?ide"; // IDE discriminator
+
+// --- Let's prepare the environment variables
+process.env.DIST_ELECTRON = join(__dirname, "../..");
+process.env.DIST = join(process.env.DIST_ELECTRON, "dist");
+process.env.PUBLIC = app.isPackaged
+  ? join(process.env.DIST, "../dist-electron/main")
+  : join(process.env.DIST_ELECTRON, "src/public");
+
+// --- Disable GPU Acceleration for Windows 7
+if (release().startsWith("6.1")) app.disableHardwareAcceleration();
+
+// --- Set application name for Windows 10+ notifications
+if (__WIN32__) app.setAppUserModelId(app.getName());
+
+// --- Make sure, only one instance is running
+if (!app.requestSingleInstanceLock()) {
+  app.quit();
+  process.exit(0);
+}
+
+// --- Register available compilers
+registerCompiler(new Z80Compiler());
+registerCompiler(new ZxBasicCompiler());
+
+loadAppSettings();
+
+// --- Store initial user settings
+mainStore.dispatch(saveUserSettingAction(appSettings.userSettings));
+
+// --- Get seeting used
+const settingsReader = createSettingsReader(mainStore);
+const allowDevTools = !!settingsReader.readSetting("devTools.allow");
+const displayIdeDevTools =
+  !!settingsReader.readSetting("devTools.ide") && allowDevTools;
+const displayEmuDevTools =
+  !!settingsReader.readSetting("devTools.emu") && allowDevTools;
+
+// --- Hold references to the renderer windows
+let ideWindow: BrowserWindow | null = null;
+let emuWindow: BrowserWindow | null = null;
+
+// --- Sign if closing the IDE window is allowed
+let allowCloseIde: boolean;
+let ideWindowStateSaved = false;
+let ideSaved: boolean;
+
+// --- Flag indicating if any virtual machine has been initialized
+let machineTypeInitialized: boolean;
+
+// --- Flag indicating the visibility of the IDE window when the EMU window was closed.
+// --- By default, the IDE window is created, loaded. However, it remains hidden.
+let ideVisibleOnClose = false;
+
+// --- Unsubscribe function for the store state change subscription
+let storeUnsubscribe: Unsubscribe | undefined;
+
+// --- URLs/URIs for the EMU and IDE windows
+const preload = join(__dirname, "../preload/index.js");
+const emuDevUrl = process.env.VITE_DEV_SERVER_URL + EMU_QP;
+const ideDevUrl = process.env.VITE_DEV_SERVER_URL + IDE_QP;
+const indexHtml = join(process.env.DIST, "index.html");
+
+// --- Store the latest build menu version to detect changes
+let lastBuildMenuVersion = 0;
+
+async function createAppWindows () {
+  // --- Reset renderer window flags used during re-activation
+  machineTypeInitialized = false;
+  allowCloseIde = false;
+  ideSaved = false;
+
+  // --- Create state manager for the EMU window
+  const emuWindowStateManager = createWindowStateManager(
+    appSettings?.windowStates?.emuWindow,
+    {
+      defaultWidth: 720,
+      defaultHeight: 540,
+      maximize: true,
+      fullScreen: true,
+      stateSaver: state => {
+        appSettings.windowStates ??= {};
+        appSettings.windowStates.emuWindow = state;
+        saveAppSettings();
+      }
+    }
+  );
+
+  // --- Create the EMU window
+  emuWindow = new BrowserWindow({
+    title: "Emu window",
+    icon: join(process.env.PUBLIC, "images/klive-logo.png"),
+    minWidth: 640,
+    minHeight: 480,
+    x: emuWindowStateManager.x,
+    y: emuWindowStateManager.y,
+    width: emuWindowStateManager.width,
+    height: emuWindowStateManager.height,
+    webPreferences: {
+      preload,
+      nodeIntegration: true,
+      contextIsolation: false,
+      webSecurity: false
+    }
+  });
+  if (displayEmuDevTools) {
+    emuWindow.webContents.toggleDevTools();
+  }
+
+  emuWindowStateManager.manage(emuWindow);
+
+  // --- Create state manager for the EMU window
+  const ideWindowStateManager = createWindowStateManager(
+    appSettings?.windowStates?.ideWindow,
+    {
+      defaultWidth: 640,
+      defaultHeight: 480,
+      maximize: false,
+      fullScreen: false,
+      stateSaver: state => {
+        appSettings.windowStates ??= {};
+        appSettings.windowStates.ideWindow = state;
+        appSettings.windowStates.showIdeOnStartup = ideWindow.isVisible();
+        saveAppSettings();
+      }
+    }
+  );
+
+  // --- Create the IDE window
+  const showIde =
+    ideVisibleOnClose || (appSettings?.windowStates?.showIdeOnStartup ?? false);
+  const maximizeIde =
+    showIde && (appSettings?.windowStates?.ideWindow?.isMaximized ?? false);
+  ideWindow = new BrowserWindow({
+    title: "Ide window",
+    icon: join(process.env.PUBLIC, "images/klive-logo.png"),
+    minWidth: 640,
+    minHeight: 480,
+    x: ideWindowStateManager.x,
+    y: ideWindowStateManager.y,
+    width: ideWindowStateManager.width,
+    height: ideWindowStateManager.height,
+    webPreferences: {
+      preload,
+      nodeIntegration: true,
+      contextIsolation: false,
+      webSecurity: false
+    },
+    show:
+      ideVisibleOnClose || (appSettings.windowStates?.showIdeOnStartup ?? false)
+  });
+  if (displayIdeDevTools && !!appSettings.windowStates?.showIdeOnStartup) {
+    ideWindow.webContents.toggleDevTools();
+  }
+
+  ideWindowStateManager.manage(ideWindow);
+
+  // --- Initialize messaging
+  registerMainToEmuMessenger(emuWindow);
+  registerMainToIdeMessenger(ideWindow);
+
+  // --- Prepare the main menu. Update items on application state change
+  Menu.setApplicationMenu(null);
+  setupMenu(emuWindow, ideWindow);
+
+  // --- Respond to state changes
+  storeUnsubscribe = mainStore.subscribe(async () => {
+    const state = mainStore.getState();
+    const loaded = state.emuLoaded;
+    if (loaded && !machineTypeInitialized) {
+      // --- Sign machine initialization is done, so we do not run into this code again
+      machineTypeInitialized = true;
+
+      // --- Set the flag indicating if we're using Windows
+      mainStore.dispatch(isWindowsAction(__WIN32__));
+
+      // --- Set saved traits
+      if (appSettings.startScreenDisplayed) {
+        mainStore.dispatch(startScreenDisplayedAction());
+      }
+      mainStore.dispatch(setThemeAction(appSettings.theme ?? "dark"));
+      mainStore.dispatch(setThemeAction(appSettings.theme ?? "dark"));
+      await setMachineType(
+        appSettings.machineId ?? "sp48",
+        appSettings.modelId,
+        appSettings.config
+      );
+      mainStore.dispatch(
+        setMachineSpecificAction(appSettings.machineSpecific ?? {})
+      );
+      mainStore.dispatch(
+        setClockMultiplierAction(appSettings.clockMultiplier ?? 1)
+      );
+      mainStore.dispatch(setSoundLevelAction(appSettings.soundLevel ?? 0.5));
+      mainStore.dispatch(showKeyboardAction(appSettings.showKeyboard ?? false));
+      mainStore.dispatch(
+        emuSetKeyboardLayoutAction(appSettings.keyboardLayout)
+      );
+      mainStore.dispatch(
+        showEmuToolbarAction(appSettings.showEmuToolbar ?? true)
+      );
+      mainStore.dispatch(
+        showEmuStatusBarAction(appSettings.showEmuStatusBar ?? true)
+      );
+      mainStore.dispatch(
+        showIdeToolbarAction(appSettings.showIdeToolbar ?? true)
+      );
+      mainStore.dispatch(
+        showIdeStatusBarAction(appSettings.showIdeStatusBar ?? true)
+      );
+      mainStore.dispatch(
+        primaryBarOnRightAction(appSettings.primaryBarRight ?? false)
+      );
+      mainStore.dispatch(
+        toolPanelsOnTopAction(appSettings.toolPanelsTop ?? false)
+      );
+      mainStore.dispatch(
+        maximizeToolsAction(appSettings.maximizeTools ?? false)
+      );
+      mainStore.dispatch(setFastLoadAction(appSettings.fastLoad ?? true));
+      if (appSettings.media[MEDIA_TAPE]) {
+        setSelectedTapeFile(appSettings.media[MEDIA_TAPE]);
+      }
+
+      // --- Set key mappings
+      if (appSettings.keyMappingFile) {
+        try {
+          const mappingSource = fs.readFileSync(
+            appSettings.keyMappingFile,
+            "utf8"
+          );
+          const mappings = parseKeyMappings(mappingSource);
+          mainStore.dispatch(
+            setKeyMappingsAction(appSettings.keyMappingFile, mappings)
+          );
+        } catch (err) {
+          // --- Intentionally ignored
+        }
+      }
+
+      if (!appSettings.startScreenDisplayed) {
+        await new Promise(r => setTimeout(r, 400));
+        if (!appSettings.startScreenDisplayed) {
+          mainStore.dispatch(displayDialogAction(FIRST_STARTUP_DIALOG_EMU));
+        }
+      }
+    }
+
+    // --- Handle build file version changes
+    if (state.project?.hasBuildFile && state.project?.buildFileVersion !== lastBuildMenuVersion) {
+      lastBuildMenuVersion = state.project?.buildFileVersion;
+      (async () => await processBuildFile())();
+    }
+
+    // --- Adjust menu items whenever the app state changes
+    setupMenu(emuWindow, ideWindow);
+  });
+
+  // --- We use a little hack here. We pass the application path value in the query parameter
+  // --- of the URL we pass to the browser windows. The IDE window will use this parameter to
+  // --- initialize the Monaco editor as soon as the IDE app starts.
+  const appPathParam = `&apppath=${encodeURI(app.getAppPath())}`;
+
+  // --- Load the contents of the browser windows. Observe, we pass the application path parameter
+  // --- to all URLs.
+  if (process.env.VITE_DEV_SERVER_URL) {
+    emuWindow.loadURL(emuDevUrl + appPathParam);
+    ideWindow.loadURL(ideDevUrl + appPathParam);
+  } else {
+    emuWindow.loadFile(indexHtml, {
+      search: EMU_QP + appPathParam
+    });
+    ideWindow.loadFile(indexHtml, {
+      search: IDE_QP + `&apppath=${process.resourcesPath}`
+    });
+  }
+  if (maximizeIde) {
+    ideWindow.maximize();
+  }
+
+  // --- Test actively push message to the Electron-Renderer
+  ideWindow.webContents.on("did-finish-load", () => {
+    ideWindow?.webContents.send(
+      "main-process-message",
+      new Date().toLocaleString()
+    );
+    if (appSettings.windowStates?.ideZoomFactor != undefined) {
+      ideWindow.webContents.setZoomFactor(
+        appSettings.windowStates?.ideZoomFactor ?? 1.0
+      );
+    }
+  });
+
+  // --- Make all links open with the browser, not with the application
+  ideWindow.webContents.setWindowOpenHandler(({ url }) => {
+    if (url.startsWith("https:")) shell.openExternal(url);
+    return { action: "deny" };
+  });
+
+  // --- Sign when IDE is focused
+  ideWindow.on("focus", () => {
+    mainStore.dispatch(ideFocusedAction(true));
+  });
+
+  // --- Sign when IDE loses the focus
+  ideWindow.on("blur", () => {
+    mainStore.dispatch(ideFocusedAction(false));
+  });
+
+  // --- Do not close the IDE (unless exiting the app), only hide it
+  ideWindow.on("close", async e => {
+    if (ideWindow?.webContents) {
+      appSettings.windowStates ??= {};
+      appSettings.windowStates.ideZoomFactor =
+        ideWindow.webContents.getZoomFactor();
+    }
+    if (allowCloseIde) {
+      // --- The emu allows closing the IDE
+      if (!ideSaved) {
+        // --- Do not allow the ide close while IDE is not saved
+        e.preventDefault();
+        // --- Make sure all edited documents are saved
+        await saveOnClose();
+
+        // --- Try to close the IDE (provided, it's not disposed)
+        ideWindow?.close();
+      }
+      return;
+    }
+
+    // --- Do not allow the IDE close, instead, hide it.
+    e.preventDefault();
+    ideWindow.hide();
+    if (appSettings.windowStates && !ideWindowStateSaved) {
+      // --- Make sure to save the last IDE settings
+      appSettings.windowStates.showIdeOnStartup = false;
+    }
+
+    // --- IDE id hidden, so it's not focused
+    mainStore.dispatch(ideFocusedAction(false));
+  });
+
+  // --- Test actively push message to the Electron-Renderer
+  emuWindow.webContents.on("did-finish-load", () => {
+    emuWindow?.webContents.send(
+      "main-process-message",
+      new Date().toLocaleString()
+    );
+    // --- Set emu zoom factor
+    if (appSettings.windowStates?.emuZoomFactor != undefined) {
+      emuWindow.webContents.setZoomFactor(
+        appSettings.windowStates?.emuZoomFactor ?? 1.0
+      );
+    }
+  });
+
+  // --- Make all links open with the browser, not with the application
+  emuWindow.webContents.setWindowOpenHandler(({ url }) => {
+    if (url.startsWith("https:")) shell.openExternal(url);
+    return { action: "deny" };
+  });
+
+  // --- Sign when EMU is focused
+  emuWindow.on("focus", () => {
+    mainStore.dispatch(emuFocusedAction(true));
+  });
+
+  // --- Sign when EMU loses the focus
+  emuWindow.on("blur", () => {
+    mainStore.dispatch(emuFocusedAction(false));
+  });
+
+  // --- Close the emu window with the IDE window
+  emuWindow.on("close", async e => {
+    if (emuWindow?.webContents) {
+      appSettings.windowStates ??= {};
+      appSettings.windowStates.emuZoomFactor =
+        emuWindow.webContents.getZoomFactor();
+    }
+    saveAppSettings();
+    if (!ideSaved) {
+      // --- Do not allow the emu close while IDE is not saved
+      e.preventDefault();
+      // --- Start saving the IDE and return back from event. The IDE will be still alive
+      await saveOnClose();
+
+      // --- Close both renderer windows (unless already disposed)
+      allowCloseIde = true;
+      ideWindow?.close();
+      emuWindow.close();
+    } else {
+      // --- The IDE is saved, so the app can be closed.
+      app.quit();
+    }
+  });
+}
+
+// --- Initialize the renderer windows whenever the app is ready to display them
+app.whenReady().then(() => {
+  createAppWindows();
+});
+
+// --- When the user is about to quit the app, allow closing the IDE window
+app.on("before-quit", e => {
+  ideWindowStateSaved = true;
+  saveAppSettings();
+});
+
+// --- Close all windows when requested so
+app.on("window-all-closed", () => {
+  storeUnsubscribe();
+  ideWindow = null;
+  emuWindow = null;
+  app.quit();
+});
+
+// --- Focus on the main window if the user tried to open another
+app.on("second-instance", () => {
+  if (ideWindow) {
+    if (ideWindow.isMinimized()) ideWindow.restore();
+    ideWindow.focus();
+  }
+});
+
+// --- Activate the windows either by displaying or re-creating them
+app.on("activate", () => {
+  const allWindows = BrowserWindow.getAllWindows();
+  if (allWindows.length) {
+    for (let i = allWindows.length - 1; i >= 0; i--) {
+      allWindows[i].focus();
+    }
+  } else {
+    // --- Let's initialize the machine type again after creating the window
+    mainStore.dispatch(unloadWindowsAction());
+
+    // --- Now, re-create the renderer windows
+    createAppWindows();
+  }
+});
+
+// https://www.electronjs.org/docs/latest/api/app#appsetaboutpaneloptionsoptions
+// app.setAboutPanelOptions(...);
+
+// --- This channel processes emulator requests and sends the results back
+ipcMain.on("EmuToMain", async (_ev, msg: RequestMessage) => {
+  let response = await forwardActions(msg);
+  if (response === null) {
+    try {
+      response = await processRendererToMainMessages(msg, emuWindow);
+    } catch (err) {
+      response = errorResponse(err.toString);
+    }
+  }
+  response.correlationId = msg.correlationId;
+  response.sourceId = "main";
+  if (emuWindow?.isDestroyed() === false) {
+    emuWindow.webContents.send("EmuToMainResponse", response);
+  }
+});
+
+// --- This channel processes ide requests and sends the results back
+ipcMain.on("IdeToMain", async (_ev, msg: RequestMessage) => {
+  let response = await forwardActions(msg);
+  if (response === null) {
+    try {
+      response = await processRendererToMainMessages(msg, ideWindow);
+    } catch (err) {
+      response = errorResponse(err.toString);
+    }
+  }
+  response.correlationId = msg.correlationId;
+  response.sourceId = "main";
+  if (ideWindow?.isDestroyed() === false) {
+    ideWindow.webContents.send("IdeToMainResponse", response);
+  }
+});
+
+// --- Process an action forward message coming from any of the renderers
+async function forwardActions (
+  message: RequestMessage
+): Promise<ResponseMessage | null> {
+  if (message.type !== "ForwardAction") return null;
+  mainStore.dispatch(message.action, message.sourceId);
+  return defaultResponse();
+}
+
+async function saveOnClose () {
+  await sendFromMainToIde({ type: "IdeSaveAllBeforeQuit" });
+  ideSaved = true;
+}
--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,94 +1,22 @@
-function domReady (
-  condition: DocumentReadyState[] = ["complete", "interactive"]
-) {
-  return new Promise(resolve => {
-    if (condition.includes(document.readyState)) {
-      resolve(true);
-    } else {
-      document.addEventListener("readystatechange", () => {
-        if (condition.includes(document.readyState)) {
-          resolve(true);
-        }
-      });
-    }
-  });
-}
+import { contextBridge } from 'electron'
+import { electronAPI } from '@electron-toolkit/preload'
 
-const safeDOM = {
-  append (parent: HTMLElement, child: HTMLElement) {
-    if (!Array.from(parent.children).find(e => e === child)) {
-      return parent.appendChild(child);
-    }
-  },
-  remove (parent: HTMLElement, child: HTMLElement) {
-    if (Array.from(parent.children).find(e => e === child)) {
-      return parent.removeChild(child);
-    }
+// Custom APIs for renderer
+const api = {}
+
+// Use `contextBridge` APIs to expose Electron APIs to
+// renderer only if context isolation is enabled, otherwise
+// just add to the DOM global.
+if (process.contextIsolated) {
+  try {
+    contextBridge.exposeInMainWorld('electron', electronAPI)
+    contextBridge.exposeInMainWorld('api', api)
+  } catch (error) {
+    console.error(error)
   }
-};
-
-/**
- * https://tobiasahlin.com/spinkit
- * https://connoratherton.com/loaders
- * https://projects.lukehaas.me/css-loaders
- * https://matejkustec.github.io/SpinThatShit
- */
-function useLoading () {
-  const className = `loaders-css__square-spin`;
-  const styleContent = `
-@keyframes square-spin {
-  25% { transform: perspective(100px) rotateX(180deg) rotateY(0); }
-  50% { transform: perspective(100px) rotateX(180deg) rotateY(180deg); }
-  75% { transform: perspective(100px) rotateX(0) rotateY(180deg); }
-  100% { transform: perspective(100px) rotateX(0) rotateY(0); }
+} else {
+  // @ts-ignore (define in dts)
+  window.electron = electronAPI
+  // @ts-ignore (define in dts)
+  window.api = api
 }
-.${className} > div {
-  animation-fill-mode: both;
-  width: 50px;
-  height: 50px;
-  background: #fff;
-  animation: square-spin 3s 0s cubic-bezier(0.09, 0.57, 0.49, 0.9) infinite;
-}
-.app-loading-wrap {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background: #282c34;
-  z-index: 9;
-}
-    `;
-  const oStyle = document.createElement("style");
-  const oDiv = document.createElement("div");
-
-  oStyle.id = "app-loading-style";
-  oStyle.innerHTML = styleContent;
-  oDiv.className = "app-loading-wrap";
-  oDiv.innerHTML = `<div class="${className}"><div></div></div>`;
-
-  return {
-    appendLoading () {
-      safeDOM.append(document.head, oStyle);
-      safeDOM.append(document.body, oDiv);
-    },
-    removeLoading () {
-      safeDOM.remove(document.head, oStyle);
-      safeDOM.remove(document.body, oDiv);
-    }
-  };
-}
-
-// ----------------------------------------------------------------------
-
-const { appendLoading, removeLoading } = useLoading();
-domReady().then(appendLoading);
-
-window.onmessage = ev => {
-  ev.data.payload === "removeLoading" && removeLoading();
-};
-
-setTimeout(removeLoading, 4999);
