6311c15e31b76173f620d497e16c955e87184636
Istvan Novak <dotneteer@hotmail.com>
2024-05-14 19:46:11 +0200

    feat: Z88DK MVP works with Klive

--- src/public/project-templates/sp48/z88-dk/code/main.c
+++ src/public/project-templates/sp48/z88-dk/code/main.c
@@ -2,7 +2,11 @@
 
 int main()
 {
-  zx_border(7);
+  zx_cls();
+  zx_border(4);
+  zx_setpaper(2);
+  zx_setattrbright(1);
+  zx_movecursorto(10,14);
   zx_printf("Welcome to Klive IDE - Using Z88DK");
   return 0;
 }
