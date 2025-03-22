c0e9a81cc8a13018f858f4610293720ce2dff1cd
Istvan Novak <dotneteer@hotmail.com>
2025-03-22 20:11:52 +0100

    docs: extend docs with directives

--- pages/z80-assembly/_meta.ts
+++ pages/z80-assembly/_meta.ts
@@ -1,6 +1,7 @@
 export default {
   "z80-assembler": "Assembler Overview",
   "language-structure": "Language Structure",
+  directives: "Directives",
   expressions: "Expressions",
   "z80-instructions": "Z80 Instructions",
   pragmas: "Pragmas",
