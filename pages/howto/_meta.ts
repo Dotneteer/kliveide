1396210119c1a7b2b95ef3203d829ec10eb969db
Istvan Novak <dotneteer@hotmail.com>
2025-03-22 14:12:17 +0100

    build: prepare to v0.40.0 (#914)

--- pages/howto/_meta.ts
+++ pages/howto/_meta.ts
@@ -2,5 +2,6 @@ export default {
   "ide-startup": "IDE startup options",
   "shortcuts": "Changing keyboard shortcuts",
   "file-extensions": "Changing default file extensions",
-  "instant-screen": "Using the Instant Screen"
+  "instant-screen": "Using the Instant Screen",
+  "always-on-top": "Keeping the Emulator always on top",
 };
