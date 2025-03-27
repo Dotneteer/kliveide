30da86462fef9589d5102e15a716fe91a97d6ea0
Istvan Novak <dotneteer@hotmail.com>
2025-03-27 19:53:46 +0100

    wip: 924

--- pages/howto/_meta.ts
+++ pages/howto/_meta.ts
@@ -1,7 +1,8 @@
 export default {
   "ide-startup": "IDE startup options",
-  "shortcuts": "Changing keyboard shortcuts",
+  shortcuts: "Changing keyboard shortcuts",
   "file-extensions": "Changing default file extensions",
   "instant-screen": "Using the Instant Screen",
   "always-on-top": "Keeping the Emulator always on top",
+  "measure-t-states": "Measuring T-states"
 };
