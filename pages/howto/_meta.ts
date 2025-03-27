e16edadfdf7fa3f256e11430e6fc68e6b952c3ff
Istvan Novak <dotneteer@hotmail.com>
2025-03-27 20:30:34 +0100

    fix: try to mend copy/paste issue (#932)

--- pages/howto/_meta.ts
+++ pages/howto/_meta.ts
@@ -1,8 +1,8 @@
 export default {
   "ide-startup": "IDE startup options",
-  "shortcuts": "Changing keyboard shortcuts",
+  shortcuts: "Changing keyboard shortcuts",
   "file-extensions": "Changing default file extensions",
   "instant-screen": "Using the Instant Screen",
   "always-on-top": "Keeping the Emulator always on top",
-  "measure-t-states": "Measuring T-states",
+  "measure-t-states": "Measuring T-states"
 };
