3a7e4e7853a2878f14c4712a0e5360bcb82fb913
Istvan Novak <dotneteer@hotmail.com>
2025-07-13 09:31:49 +0200

    Revert "docs: refactor documentation (#1004)" (#1013)
    This reverts commit 46591ae95056cdf572d0b1c11a1248cddfa3b8ec.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,7 @@
 import type { AppProps } from 'next/app'
-import { ThemeProvider } from 'next-themes'
 import "nextra-theme-docs/style.css";
 import "./custom.css";
  
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  )
+  return <Component {...pageProps} />
 }
--- pages/mdx-components.tsx
+++
@@ -1,9 +0,0 @@
-import type { MDXComponents } from 'mdx/types'
-import { ClickableImage } from '../page-components'
-
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return {
-    ...components,
-    ClickableImage,
-  }
-}
