7c1e4cf6ac6db1d11ec73ccd990d8e31bca72bd8
Istvan Novak <dotneteer@hotmail.com>
2025-07-13 08:36:06 +0200

    fix: docs build 1

--- pages/mdx-components.tsx
+++ pages/mdx-components.tsx
@@ -1,9 +1,16 @@
 import type { MDXComponents } from 'mdx/types'
 import { ClickableImage } from '../page-components'
 
+// This file should not be a page, it should be used as a components provider for MDX
+// Moving this function to a non-page location would be better, but for a quick fix:
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
     ClickableImage,
   }
+}
+
+// Add default export to satisfy Next.js page requirements
+export default function MDXComponentsPage() {
+  return null;
 }
