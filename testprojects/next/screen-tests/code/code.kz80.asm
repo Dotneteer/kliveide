55d33b1604fecf2b66fb336994f591ea2bfc5a5e
Istvan Novak <dotneteer@hotmail.com>
2026-01-25 15:58:49 +0100

    Update the ZX Spectrum Next documentation

--- testprojects/next/screen-tests/code/code.kz80.asm
+++ testprojects/next/screen-tests/code/code.kz80.asm
@@ -6,13 +6,38 @@
 .savenex file "screen-tests.nex"
 .savenex core "3.1.0"
 
+; Unbanked code in bank 2 at $8000
 main:
-    ; Entry point code
-    ld a,3
-    out ($fe),a
+    ; 
+    ; Save the current MMU 5 value ($A000-$BFFF)
+    ld a,$55
+    ld bc,$243b
+    out (c),a
+    inc b
+    in a,(c)
+    push af
+    ;
+    ; Page in the first 8K of 16K Bank $20 to $A000-$BFFF
+    di
+    nextreg $55,$40
+    ;
+    ; Invoke the subrouting in Bank $20
+    call SetBorder
+    ;
+    ; Restore the old MMU 5 value
+    pop af
+    nextreg $55,a
+    ei
 trap 
     jr $
 
-TX_Hello
-    .defm "Hello"
-TX_Hello_len = $ - TX_Hello
+
+; Explicit bank $20 code
+.bank $20
+.org $0000
+.disp $a000
+
+SetBorder
+    ld a,3
+    out ($fe),a
+    ret
