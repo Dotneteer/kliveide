1735c68f92c77edf44e7110166ddc9b554b4c096
Istvan Novak <dotneteer@hotmail.com>
2026-04-11 08:14:03 +0200

    wip: rudimentary plan added

--- src/renderer/appIde/utils/compiler-utils.ts
+++ src/renderer/appIde/utils/compiler-utils.ts
@@ -1,4 +1,4 @@
-import { CompilerOutput, KliveCompilerOutput } from "@abstractions/CompilerInfo";
+import { CompilerOutput, DebuggableOutput, KliveCompilerOutput, SourceLevelDebugInfo } from "@abstractions/CompilerInfo";
 
 /**
  * Type guard that checks if the specified output can be used for code injection
@@ -16,4 +16,14 @@ export function isInjectableCompilerOutput(output: KliveCompilerOutput): output 
  */
 export function isDebuggableCompilerOutput(output: KliveCompilerOutput): output is CompilerOutput {
   return (output as any)?.segments && (output as any)?.sourceFileList;
+}
+
+/**
+ * Type guard: checks whether the compiler output contains source-level
+ * debug information (SourceLevelDebugInfo), enabling statement-level stepping.
+ */
+export function hasSourceLevelDebug(
+  output: KliveCompilerOutput
+): output is DebuggableOutput & { sourceLevelDebug: SourceLevelDebugInfo } {
+  return isDebuggableCompilerOutput(output) && !!(output as DebuggableOutput).sourceLevelDebug;
 }
