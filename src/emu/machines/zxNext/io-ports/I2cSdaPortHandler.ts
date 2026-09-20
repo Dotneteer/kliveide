23cc4f8a4022df80449ec33a473335cee8396cbe
Istvan Novak <dotneteer@hotmail.com>
2026-09-20 13:47:18 +0200

    Complete the ZX Spectrum Next TypeScript version removal

--- src/emu/machines/zxNext/io-ports/I2cSdaPortHandler.ts
+++
@@ -1,9 +0,0 @@
-import type { IZxNextMachine } from "@renderer/abstractions/IZxNextMachine";
-
-export function readI2cSdaPort(machine: IZxNextMachine): (port: number) => number {
-  return () => machine.i2cDevice.readSdaPort();
-}
-
-export function writeI2cSdaPort(machine: IZxNextMachine): (port: number, value: number) => void {
-  return (_, value) => machine.i2cDevice.writeSdaPort(value);
-}
--- src/emu/machines/zxNext/io-ports/UartRxPortHandler.ts
+++
@@ -1,9 +0,0 @@
-import type { IZxNextMachine } from "@renderer/abstractions/IZxNextMachine";
-
-export function readUartRxPort(machine: IZxNextMachine): (port: number) => number {
-  return () => machine.uartDevice.readRxPort();
-}
-
-export function writeUartRxPort(machine: IZxNextMachine): (port: number, value: number) => void {
-  return (_, value) => machine.uartDevice.writeRxPort(value);
-}
